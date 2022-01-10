1f50b1896dcdd05763bbab8ebdf8a7f678173cc4
AP <ashis.n.patel@gmail.com>
2022-01-09 21:56:26 -0600

    Add starting code for menu and ordering

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ MENU = {
             "water": 50,
             "coffee": 18,
         },
-        "cost": 1.5,
+        "cost": 1.50,
     },
     "latte": {
         "ingredients": {
@@ -20,7 +20,7 @@ MENU = {
             "milk": 150,
             "coffee": 24,
         },
-        "cost": 2.5,
+        "cost": 2.50,
     },
     "cappuccino": {
         "ingredients": {
@@ -28,7 +28,7 @@ MENU = {
             "milk": 100,
             "coffee": 24,
         },
-        "cost": 3.0,
+        "cost": 3.00,
     }
 }
 
@@ -39,7 +39,7 @@ resources = {
 }
 
 def redirect():
-    input("Press enter to return to the main menu.")
+    input("\nPress enter to return to the main menu.")
     return coffee_machine()
 
 def coffee_machine():
@@ -55,11 +55,25 @@ Available Options:
     )
 
     machine_action = input("What would you like to do (1/2/3)? ")
-
+    print("")
+    
     if(machine_action == '1'):
-        print("Here's the menu")
+        print("Menu:")
+        item_number = 1
+        for beverage in MENU:
+            cost = "{:.2f}".format(MENU[beverage]["cost"])
+            print(f"{item_number}. {beverage.title()} - ${cost}")
+        beverage_choice = input("What would you like to order (1/2/3)? ")
+        # check if machine has enough resources to make drink
+        
+        # prompt for money
+
+        # update resources 
+
+        # return change
     elif(machine_action == '2'):
-        print(resources)
+        for resource in resources:
+            print(f"{resource.title()}: {resources[resource]}")
         redirect()
     elif(machine_action == '3'):
         print("Bye-bye! (^^)b ")
