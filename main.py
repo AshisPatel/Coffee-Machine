3bcdf319b30ba5d08849874939a7bbcf35e11704
AP <ashis.n.patel@gmail.com>
2022-01-10 22:30:23 -0600

    Begin working on change input function

--- main.py
+++ main.py
@@ -38,13 +38,19 @@ resources = {
     "coffee": 100,
 }
 
-def redirect():
-    input("\nPress enter to return to the main menu.")
-    return coffee_machine()
 
-def coffee_machine():
-
+def redirect(menu):
+    input(f"\nPress enter to return to the {menu}.")
     clearConsole()
+    print("")
+    if menu == "main menu":
+        return main_menu()
+    elif menu == "beverage menu":
+        return order_beverage()
+
+def main_menu():
+
+    
     print(
 """
 Available Options: 
@@ -58,46 +64,62 @@ Available Options:
     print("")
     
     if(machine_action == '1'):
-        print("Menu:")
-        item_number = 1
-        for beverage in MENU:
-            cost = "{:.2f}".format(MENU[beverage]["cost"])
-            print(f"{item_number}. {beverage.title()} - ${cost}")
-            item_number +=1
-        beverage_choice = int(input("What would you like to order (1/2/3)? ").lower())
-        if beverage_choice == 1:
-            beverage_choice = "espresso"
-        elif beverage_choice == 2:
-            beverage_choice = "latte"
-        elif beverage_choice == 3:
-            beverage_choice = "cappuccino"
-        else:
-            print("Sorry, that is not a valid item!")
-            redirect()
-        # check if machine has enough resources to make drink
-        selected_beverage = MENU[beverage_choice]
-        beverage_ingredients = selected_beverage["ingredients"]
-        sufficient_resources = True
-        for ingredient in beverage_ingredients:
-            if beverage_ingredients[ingredient] > resources[ingredient]:
-                sufficient_resources = False
-                print("Sorry, machine does not have sufficient resources to make that beverage.")
-        
-        # prompt for money
-
-        # update resources 
-
-        # return change
+        return order_beverage()
     elif(machine_action == '2'):
         for resource in resources:
             print(f"{resource.title()}: {resources[resource]}")
-        redirect()
+        redirect("main menu")
     elif(machine_action == '3'):
         print("Bye-bye! (^^)b ")
         exit()
     else: 
         print("Not a valid option.\n")
-        redirect()
+        redirect("main menu")
     
 
-coffee_machine()
+def order_beverage():
+    print("Menu:")
+    item_number = 1
+    for beverage in MENU:
+        cost = "{:.2f}".format(MENU[beverage]["cost"])
+        print(f"{item_number}. {beverage.title()} - ${cost}")
+        item_number +=1
+    beverage_choice = int(input("\nWhat would you like to order (1/2/3)? ").lower())
+    if beverage_choice == 1:
+        beverage_choice = "espresso"
+    elif beverage_choice == 2:
+        beverage_choice = "latte"
+    elif beverage_choice == 3:
+        beverage_choice = "cappuccino"
+    else:
+        print("\nSorry, that is not a valid item!")
+        redirect("beverage menu")
+    # check if machine has enough resources to make drink
+    selected_beverage = MENU[beverage_choice]
+    beverage_ingredients = selected_beverage["ingredients"]
+    sufficient_resources = True
+    for ingredient in beverage_ingredients:
+        if beverage_ingredients[ingredient] > resources[ingredient]:
+            sufficient_resources = False
+            print("Sorry, machine does not have sufficient resources to make that beverage.")
+            redirect("main menu")
+    
+    # prompt for money
+    beverage_cost = selected_beverage["cost"]
+    # update resources 
+    while beverage_cost > 0:
+        
+    # return change
+
+def convert_change(cost):
+    quarters = input("Enter number of quarters: ")
+    dimes = input("Enter number of dimes: ")
+    nickels = input("Enter number of nickels: ")
+    pennies = input("Enter number of pennies: ")
+
+    return (quarters * 25 + dimes * 10 + nickels * 5 + pennies)
+
+
+clearConsole()
+main_menu()
+
